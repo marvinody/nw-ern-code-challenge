0e81ce116cf6d0f860f4f45d6df067b534dfd1b2
Manuel Bugallo <marvinody@gmail.com>
2020-01-18 19:27:07 -0500

    bugfix: fixes small timing issue on timeline checking

--- specs/timeline.js
+++ specs/timeline.js
@@ -4,9 +4,13 @@ const rp = require('request-promise')
 // bad name but 12hr -> 24hr clock
 // also changes from string to num
 const twelveToTwentyFour = (time) => {
+  if (time === '12am') {
+    // edge case since we don't say 0am
+    return 0
+  }
   const hour = Number(time.slice(0, -2)) // cut the last 2 chars off
   const str = time.slice(-2) // get the last 2 chars
-  if (str === 'pm') {
+  if (str === 'pm' && hour < 12) {
     return (hour + 12) % 24
   }
   return hour
@@ -32,8 +36,18 @@ describe('DarkSky Timeline readings', () => {
     const now = new Date()
     const TIME_BETWEEN_READINGS = 2
 
-    // start at one cause we checked 0 up there
-    for (let i = 1; i < hours.length; i++) {
+    // sometimes, the ui displays the numbers starting 1 hour ahead, sometimes 2
+    // so we'll use the second time slot to calibrate our readings
+    // and then the check the rest for real
+    const firstHourTime = twelveToTwentyFour(hours[1].getText())
+    if ((now.getHours() + 1) % 24 === firstHourTime) {
+      // then we can assume, the offset starts by 1 and are misaligned.
+      // so let's fix it
+      now.setHours(firstHourTime)
+    }
+
+    // start at two cause we checked 0 up there and used 1 for calibration
+    for (let i = 2; i < hours.length; i++) {
       const offset = new Date(now) // copy of before
       // set it to the expected time
       // this will handle wrap around if we add to like 25 or something
